63de71988c7e7bf663e0f935ade881490fb52258
Manish Shirke <buntyshirke22@gmail.com>
2020-01-06 18:31:51 +0530

    read blade changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>SR.No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(isset($user))
+                        @foreach($user as $d)
+                        <tr>
+                            <td>{{ $d->id }}</td>
+                            <td>{{ $d->name }}</td>
+                            <td>{{ $d->email }}</td>
+                            <td>{{ $d->password }}</td>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
+
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <!-- You are logged in! -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
